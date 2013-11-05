10a77de05bae6065c43025be4b716e893029e7e6
Kakashidinho <lehoangq@gmail.com>
2013-11-05 22:33:15 +0800

    +added web server to test2. dont need to use startSimpleHTTPServer script anymore

--- test2/test2_server.js
+++ test2/test2_server.js
@@ -2,6 +2,22 @@
 
 require("./../src/Server.js");
 
+//start HTTP Server to serve web contents
+var express = require('express');
+var http = require('http');
+			
+var app = express();
+var httpServer = http.createServer(app);
+httpServer.listen(2001, '0.0.0.0');
+app.use(express.static(__dirname + '/../'));
+app.use(express.static(__dirname));
+
+app.get('/', function(req,res) {
+  console.log("get ('/')");
+  res.sendfile('test2.html');
+});
+
+/*------start game server------*/
 var server = new Server();
 server.start();
 server.beginStartGame("init2.xml");
