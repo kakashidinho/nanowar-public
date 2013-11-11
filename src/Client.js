685cb07da35f8db25089a70c46d480d37f5442f6
jasonpeng <jasonpeng.boy@gmail.com>
2013-11-11 12:25:03 +0800

    use document.body.clientWidth as default

--- src/Client.js
+++ src/Client.js
@@ -22,8 +22,8 @@ function Client(canvasElementID)
 		
 	this.canvas = document.getElementById(canvasElementID);
 	
-	canvas.width  = window.innerWidth || document.body.clientWidth;
-	canvas.height  = window.innerHeight || document.body.clientHeight;
+	canvas.width  = document.body.clientWidth || window.innerWidth;
+	canvas.height  = document.body.clientHeight || window.innerHeight;
 }
 
 Client.prototype.setInitFile = function(xmlFile){
